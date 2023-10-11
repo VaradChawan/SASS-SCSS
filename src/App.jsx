59f61266e8445b6764c44b560ef31d712d787c18
VaradChawan <varadchawan@gmail.com>
2023-10-11 16:46:10 +0530

    SCSS in action

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import styles from './App.module.scss';
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className={styles.App}>Hello World 
+    <div><br/> <br/>Welcome to SCSS World </div>
+    </div>
+    <div className={styles.wwww}>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia suscipit beatae officiis aliquam, delectus repellendus iure asperiores architecto sequi iusto praesentium, assumenda provident porro nemo illo eveniet fugit. A, earum.</p>
+      <h2>Hello world</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
